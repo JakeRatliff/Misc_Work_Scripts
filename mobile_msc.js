8b20643dcedfd0b5d2fca98cee2b065d5537228c
Jake Ratliff <wrjake@gmail.com>
2019-01-24 13:28:30 -0500

    Create mobile_msc.js

---
+++ mobile_msc.js
@@ -0,0 +1,6 @@
+if(window.innerWidth < 600){
+	var x = document.getElementById('g0501_desktop');
+	var y = document.getElementById('g0501_mobile');
+	x.style.display = "none";
+	y.style.display = "block";
+}
